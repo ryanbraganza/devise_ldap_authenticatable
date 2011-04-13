97222a27ac6c230f47c16d48402b7253b9cd0b60
Ubuntu.10.10 <clyfe@ubuntu.(none)>
2011-04-13 03:05:39 -0700

    fix typo

--- lib/devise_ldap_authenticatable/schema.rb
+++ lib/devise_ldap_authenticatable/schema.rb
@@ -3,7 +3,7 @@ Devise::Schema.class_eval do
   def ldap_authenticatable(options={})
     null = options[:null] || false
 
-    apply_devise_schema :is_ldap, :boolean, :null => null, :defult => false
+    apply_devise_schema :is_ldap, :boolean, :null => null, :default => false
   end
  
 end
